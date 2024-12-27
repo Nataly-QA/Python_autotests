d8e0e39e1271dbdd041419d9c026b117d73d8d3e
Nataly-QA <mk7407863@gmail.com>
2024-12-27 15:10:49 +0300

    Update test_pokemons.py

--- PythonProjects/tests/test_pokemons.py
+++ PythonProjects/tests/test_pokemons.py
@@ -17,7 +17,3 @@ def test_part_of_response():
     response_get= requests.get(url = f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})
     assert response_get.json()["data"][0]["trainer_name"] == "Tasha"
 
-@pytest.mark.parametrize('key,value', [('trainer_name', 'Tasha'), ('id', TRAINER_ID)])
-def test_parametrize(key, value):
-    response_parametrize= requests.get(url = f'{URL}/trainers', params = {'trainer_id': TRAINER_ID})
-    assert response_parametrize.json()["data"][0][key] == value 
