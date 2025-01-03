98abdb7b30fa18f27233f9a06aa2eb89684db718
Nataly-QA <mk7407863@gmail.com>
2025-01-03 12:30:09 +0300

    Update main.py

--- PythonProjects/main.py
+++ PythonProjects/main.py
@@ -1,5 +1,7 @@
 import requests
 
+# {'id': '12181', 'trainer_token': '616cbfa52227263a76d870b4adb8548c', 'trainer_name': 'Tasha'}
+
 URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = '616cbfa52227263a76d870b4adb8548c'
 HEADER = {'Content-Type': 'application/json', 'trainer_token' : TOKEN}
@@ -8,6 +10,7 @@ create_body = {
     "name": "Гудвин",
     "photo_id": 522
 }
+response_create = None
 
 response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = create_body)
 print(response_create.text)
@@ -15,7 +18,7 @@ print(response_create.text)
 pokemon_id = response_create.json()['id']
 
 change_body = {
-    "pokemon_id": '173909',
+    "pokemon_id": pokemon_id,
     "name": "Зубастик",
     "photo_id": 521
 }
@@ -25,7 +28,7 @@ print(response_change.text)
 
 
 pokeball_body = {
-    "pokemon_id": '173909'
+    "pokemon_id": pokemon_id
 }
 
 response_pokeball = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = pokeball_body)
